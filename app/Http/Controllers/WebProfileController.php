4ec85abe5c31477ac25ea3b164eadb9bb22b53f5
FAOZI <faozimipa@gmail.com>
2017-03-05 21:20:58 +0700

    fixing validator

--- app/Http/Controllers/WebProfileController.php
+++ app/Http/Controllers/WebProfileController.php
@@ -6,32 +6,57 @@ use App\Models\Contact;
 use Illuminate\Http\Request;
 use Alert;
 use App\Models\Subscribe;
+use Illuminate\Support\Facades\Validator;
 
 class WebProfileController extends Controller
 {
     public function index ()
     {
-
-      //  alert()->success('You have been logged in.', 'Happy Nice Day!');
         return view('themes.profile.master');
 
     }
 
     public function post_subscribe(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|unique:subscribes',
+        ]);
+        if ($validator->fails()) {
+
+            Alert::warning('Trimakasih', 'Email Anda telah terdaftar!');
+
+            return redirect()->back();
+
+        }
+
         $data['email'] = $request->email;
         $save = Subscribe::create($data);
+
         if($save){
             Alert::success('OK', 'Subscribe Success!');
         }else{
             Alert::error('Subscribe Error', 'Opps...!');
         }
+
         return redirect()->back();
 
     }
 
     public function post_message (Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'name' => 'required',
+            'message' => 'required'
+        ]);
+        if ($validator->fails()) {
+
+            Alert::warning('Trimakasih', 'Ada isian yang belum diisi!');
+
+            return redirect()->back();
+
+        }
+
         $data['email']= $request->email;
         $data['website'] = $request->website;
         $data['message'] = $request->message;
